4f6905e821c5a70af23c6eea0fb63c9abd201e7c
Gabriel <rgvo2101@gmail.com>
2021-02-09 20:28:23 -0600

    datos1

---
+++ TiposDeDatos/TiposDeDatos.py
@@ -0,0 +1,9 @@
+# Tipos de Datos
+nombrePersona = 'Roberto'
+edadPersona = 21
+direccionPersona = "Las Isabeles ###"
+telefonoPersona = 5536717851
+print("Nombre: " + nombrePersona)
+print("Edad: " + str(edadPersona))
+print("Dirección: " + direccionPersona)
+print("Teléfono: " + str(telefonoPersona))

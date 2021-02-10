07414a5078b851fe479e4943197277d5f6c1173f
Gabriel <rgvo2101@gmail.com>
2021-02-09 20:31:41 -0600

    datos2

--- TiposDeDatos/TiposDeDatos.py
+++ TiposDeDatos/TiposDeDatos.py
@@ -3,7 +3,11 @@ nombrePersona = 'Roberto'
 edadPersona = 21
 direccionPersona = "Las Isabeles ###"
 telefonoPersona = 5536717851
+valorDecimal = 4.54323456
+valorBooleano = True
 print("Nombre: " + nombrePersona)
 print("Edad: " + str(edadPersona))
 print("Dirección: " + direccionPersona)
 print("Teléfono: " + str(telefonoPersona))
+print("PruebaDecimal: " + str(valorDecimal))
+print("PruebaBooleano: " + str(valorBooleano))

1115ebc8e766eff1cf2a2ee9ea98e14835113f90
Gabriel <rgvo2101@gmail.com>
2021-02-10 21:17:59 -0600

    condicionales2

--- Condicionales/Ejercicio.py
+++ Condicionales/Ejercicio.py
@@ -10,15 +10,28 @@ promedio = sumacalificaciones/3
 print("El promedio es: " + str(promedio))
 if(float(c1) >= 6):
     print("La primer calificación si aprobó")
+elif(float(c1) < 0 ):
+    print("Calificación incorrecta")
 else:
     print("La primer calificación no aprobó")
     
 if(float(c2) >= 6):
     print("La segunda calificación si aprobó")
+elif(float(c2) < 0 ):
+    print("Calificación incorrecta")
 else:
     print("La segunda calificación no aprobó") 
        
 if(float(c3) >= 6):
     print("La tercer calificación si aprobó")
+elif(float(c3) < 0 ):
+    print("Calificación incorrecta")    
 else:
-    print("La tercer calificación no aprobó")    
+    print("La tercer calificación no aprobó")
+
+if(float(promedio) >= 6):
+    print("El promedio aprobó")
+elif(float(promedio) < 0 ):
+    print("Promedio incorrecto")    
+else:
+    print("El promedio no aprobó")        

42c990e3f393365dfcadb1adcccf268fc0f5d615
Gabriel <rgvo2101@gmail.com>
2021-02-10 20:31:20 -0600

    arreglos1

---
+++ Arreglos/Arreglos.py
@@ -0,0 +1,15 @@
+#Declarar un arreglo
+arreglo = ["Si" , 1 , 121.32 , False , "Hola" , True , 3 , 2]
+arreglo[0] = "México"
+#arreglo[1] = 123
+#arreglo[2] = -23.21321 
+#arreglo[3] = True
+#arreglo[4] = "Sabritas"
+#arreglo[5] = False
+#arreglo[6] = 321-123
+#arreglo[7] = 23*232 
+#Sangría o Identación
+for item in arreglo:
+    print(item)
+
+print(type(arreglo))

067fbdf2fb3487f245f326562d3551988f5a8e27
Gabriel <rgvo2101@gmail.com>
2021-02-11 20:09:42 -0600

    diccionario2

--- Diccionarios/Diccionarios.py
+++ Diccionarios/Diccionarios.py
@@ -1,8 +1,13 @@
-diccionario = {"Nombre": "Juan","Apellido":"Perez","Ocupación":"Médico","Dirección":{"Calle":"Villada","Numero Exterior": 32,
+diccionario = {"Nombre": "Juan","Apellido":"Perez","Ocupación":"Médico","Dirección":{"Calle":"Villada","Número Exterior": 32,
 "Número Interior": 1,"Código Postal": 43432,"Municipio/Alcaldia":"Iztacalco","Estado":"Ciudad de México"},"Música":"Pop"}
 print(diccionario)
 print(diccionario["Nombre"])
 print(diccionario["Apellido"])
 print(diccionario["Ocupación"])
-print(diccionario["Dirección"])
+print(diccionario["Dirección"]["Calle"])
+print(diccionario["Dirección"]["Número Exterior"])
+print(diccionario["Dirección"]["Número Interior"])
+print(diccionario["Dirección"]["Código Postal"])
+print(diccionario["Dirección"]["Municipio/Alcaldia"])
+print(diccionario["Dirección"]["Estado"])
 print(diccionario["Música"])

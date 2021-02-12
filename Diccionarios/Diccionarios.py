db9e70f80aaf739a625ceb383bc0d365c37c91e9
Gabriel <rgvo2101@gmail.com>
2021-02-11 20:04:55 -0600

    diccionario1

---
+++ Diccionarios/Diccionarios.py
@@ -0,0 +1,8 @@
+diccionario = {"Nombre": "Juan","Apellido":"Perez","Ocupación":"Médico","Dirección":{"Calle":"Villada","Numero Exterior": 32,
+"Número Interior": 1,"Código Postal": 43432,"Municipio/Alcaldia":"Iztacalco","Estado":"Ciudad de México"},"Música":"Pop"}
+print(diccionario)
+print(diccionario["Nombre"])
+print(diccionario["Apellido"])
+print(diccionario["Ocupación"])
+print(diccionario["Dirección"])
+print(diccionario["Música"])
--- Ejercicios/Ejercicio1.py
+++ Ejercicios/Ejercicio1.py
@@ -1,4 +1,4 @@
-print("Escribe las tres calificaciones: ")
+print("Escribe las tres calificaciones")
 print("Primera calificación: ")
 c1 = input()
 print("Segunda calificación: ")
@@ -8,30 +8,30 @@ c3 = input()
 sumacalificaciones = float(c1) + float(c2) + float(c3)
 promedio = sumacalificaciones/3
 print("El promedio es: " + str(promedio))
-if(float(c1) >= 6):
+if(float(c1) >= 6 and float(c1)<=10):
     print("La primer calificación si aprobó")
-elif(float(c1) < 0 ):
-    print("Calificación incorrecta")
+elif(float(c1) < 0 or float(c1)>10):
+    print("Primer calificación incorrecta")
 else:
     print("La primer calificación no aprobó")
     
-if(float(c2) >= 6):
+if(float(c2) >= 6 and float(c2)<=10):
     print("La segunda calificación si aprobó")
-elif(float(c2) < 0 ):
-    print("Calificación incorrecta")
+elif(float(c2) < 0 or float(c2)>10 ):
+    print("Segunda calificación incorrecta")
 else:
     print("La segunda calificación no aprobó") 
        
-if(float(c3) >= 6):
+if(float(c3) >= 6 and float(c3)<=10):
     print("La tercer calificación si aprobó")
-elif(float(c3) < 0 ):
-    print("Calificación incorrecta")    
+elif(float(c3) < 0 or float(c3)>10):
+    print("Tercer calificación incorrecta")    
 else:
     print("La tercer calificación no aprobó")
 
-if(float(promedio) >= 6):
+if(float(promedio) >= 6 and float(promedio)<=10):
     print("El promedio aprobó")
-elif(float(promedio) < 0 ):
+elif(float(promedio) < 0 or float(promedio)>10 ):
     print("Promedio incorrecto")    
 else:
-    print("El promedio no aprobó")        
+    print("El promedio no aprobó")
